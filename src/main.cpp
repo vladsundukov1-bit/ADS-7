d19aa39eed15b5a7742f04b416006b105dfb4453
vladsundukov1-bit <vladsundukov1@gmail.com>
2026-05-30 23:37:22 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,23 +8,26 @@
 #include "train.h"
 
 int main() {
-  #ifndef INCLUDE_TRAIN_H_
-#define INCLUDE_TRAIN_H_
-
-class Train {
- private:
-  struct Car {
-    bool light;
-    Car *next;
-    Car *prev;
-  };
-  int countOp;
-  Car *first;
- public:
-  Train();
-  void addCar(bool light);
-  int getLength();
-  int getOpCount();
-};
-#endif
+    std::cout << "n\tall_off\tall_on\trandom\n";
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+    std::ofstream resultFile("result/experiment_data.csv");
+    resultFile << "n,all_off,all_on,random\n";
+    for (int n = 10; n <= 200; n += 10) {
+        Train trainOff;
+        for (int i = 0; i < n; ++i) trainOff.addCar(false);
+        trainOff.getLength();
+        int opsOff = trainOff.getOpCount();
+        Train trainOn;
+        for (int i = 0; i < n; ++i) trainOn.addCar(true);
+        trainOn.getLength();
+        int opsOn = trainOn.getOpCount();
+        Train trainRand;
+        for (int i = 0; i < n; ++i) trainRand.addCar(std::rand() % 2);
+        trainRand.getLength();
+        int opsRand = trainRand.getOpCount();
+        std::cout << n << "\t" << opsOff << "\t" << opsOn << "\t" << opsRand << "\n";
+        resultFile << n << "," << opsOff << "," << opsOn << "," << opsRand << "\n";
+    }
+    resultFile.close();
+    return 0;
 }
